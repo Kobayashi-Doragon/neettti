98ee3be5dbf4d6859a05f1307c64ae91b91aacc1
uutann <uutann9916@gmail.com>
2020-02-05 13:50:57 +0900

    push確認

--- app.py
+++ app.py
@@ -1,42 +1,44 @@
 # ルートによってメソッドを呼び出す
 
-from flask import Flask,render_template,request
+from flask import Flask, render_template, request
 import methods
+
 # import sql
 
 app = Flask(__name__)
-player=methods.player()
-#push確認
-
+player = methods.player()
 
 
 # 初めのログイン画面表示
 @app.route("/")
 def login_screen():
-    return render_template("login.html",message="")
+    return render_template("login.html", message="")
+
 
 # ログインボタンでログインしゲーム画面へ
-@app.route("/login",methods=["POST"])
+@app.route("/login", methods=["POST"])
 def login():
     # 入力データを引数にloginメソッド呼ぶ
-    id=request.form["id"]
-    password=request.form["password"]
+    id = request.form["id"]
+    password = request.form["password"]
     # DBと同じならゲーム画面へ、違うならもう一度入力してもらう
-    if(player.login(id,password)):
-        return render_template("game.html",money=player.money,fatigue=player.mother_fatigue,time="仕事前")
+    if (player.login(id, password)):
+        return render_template("game.html", money=player.money, fatigue=player.mother_fatigue, time="仕事前")
     else:
-        return render_template("login.html",message="ユーザIDもしくはパスワードが間違っています")
+        return render_template("login.html", message="ユーザIDもしくはパスワードが間違っています")
+
 
 # 入力を元にアカウント追加
-@app.route("/create",methods=["POST"])
+@app.route("/create", methods=["POST"])
 def create_account():
     id = request.form["id"]
     password = request.form["password"]
 
-    if(player.create(id,password)):
-        return render_template("game.html",money=player.money,fatigue=player.mother_fatigue,time="仕事前")
+    if (player.create(id, password)):
+        return render_template("game.html", money=player.money, fatigue=player.mother_fatigue, time="仕事前")
     else:
-        return render_template("login.html",message="ユーザIDが既に使用されています")
+        return render_template("login.html", message="ユーザIDが既に使用されています")
+
 
 # セーブしてログイン画面に戻る
 @app.route("/logout")
@@ -44,34 +46,43 @@ def logout():
     # セーブ
     return render_template("login.html")
 
+
 @app.route("/talk")
 def talk():
     # トーク
-    answer=player.talk()
-    return render_template("game.html",message="話しかけた",neet_answer=answer,money=player.money,fatigue=player.mother_fatigue,time="仕事前")
+    answer = player.talk()
+    return render_template("game.html", message="話しかけた", neet_answer=answer, money=player.money,
+                           fatigue=player.mother_fatigue, time="仕事前")
+
 
 @app.route("/feed")
 def feed():
     # feed
     food_id = request.args.get("food_id")
 
-    return render_template("game.html",message=food_id+"を与えた",neet_answer="",money=player.money,fatigue=player.mother_fatigue,time="仕事前")
+    return render_template("game.html", message=food_id + "を与えた", neet_answer="", money=player.money,
+                           fatigue=player.mother_fatigue, time="仕事前")
+
 
 @app.route("/buy")
 def buy():
     # buy
     buy_id = request.args.get("buy_id")
-    return render_template("game.html",message=buy_id+"を与えた",neet_answer="",money=player.money,fatigue=player.mother_fatigue,time="仕事前")
+    return render_template("game.html", message=buy_id + "を与えた", neet_answer="", money=player.money,
+                           fatigue=player.mother_fatigue, time="仕事前")
+
 
 @app.route("/work")
 def work():
     # work
-    return render_template("game.html",message="仕事に行った",money=player.money,fatigue=player.mother_fatigue,time="仕事前")
+    return render_template("game.html", message="仕事に行った", money=player.money, fatigue=player.mother_fatigue, time="仕事前")
+
 
 @app.route("/sleep")
 def sleep():
     # sleep
-    return render_template("game.html",message="寝た",money=player.money,fatigue=player.mother_fatigue,time="仕事前")
+    return render_template("game.html", message="寝た", money=player.money, fatigue=player.mother_fatigue, time="仕事前")
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
